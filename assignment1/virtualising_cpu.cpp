363672ffc8cf3b9114874f47a5a3047be2031cca
anubhav100rao <anubhav100rao@gmail.com>
2022-02-28 10:16:10 +0530

    updated

--- assignment1/virtualising_cpu.cpp
+++ assignment1/virtualising_cpu.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<windows.h>
+#include<iostream>
 
-int main(int argc, char** argv) { // vector<string>
+int main(int argc, char** argv) {
     
     if(argc < 2) {
         printf("please enter some command line argument\n");
         return 0;
     }
+
     while(1) {
         printf("%s\n", argv[1]);
         Sleep(1000);
